b58ce2f8a8294c536c1a19bb8aa87318714231bb
rafael <rafael.piedra@epn.edu.ec>
2024-01-03 15:07:19 -0500

    clase 3 de enero 2024

--- 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/ACicloVida.kt
+++ 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/ACicloVida.kt
@@ -3,55 +3,78 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
+
 class ACicloVida : AppCompatActivity() {
-
     var textoGlobal = ""
-    fun mostrarSnackBar(texto:String){
+    fun mostrarSnackbar(texto:String){
         textoGlobal = textoGlobal + " " + texto
-        Snackbar.make(
-            findViewById(R.id.cl_ciclo_vida),
-            textoGlobal,
-            Snackbar.LENGTH_INDEFINITE
-        ).show()
+        Snackbar
+            .make(
+                findViewById(R.id.cl_ciclo_vida), // view
+                textoGlobal, // texto
+                Snackbar.LENGTH_INDEFINITE // tiempo
+            )
+            .show()
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
-        mostrarSnackBar("OnCreate")
+        mostrarSnackbar("OnCreate")
     }
-
     override fun onStart() {
         super.onStart()
-        mostrarSnackBar("onStart")
+        mostrarSnackbar( "onStart")
     }
-
-
     override fun onResume() {
         super.onResume()
-        mostrarSnackBar("onResume")
+        mostrarSnackbar( "onResume")
     }
-
-    override fun onPause() {
-        super.onPause()
-        mostrarSnackBar("onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mostrarSnackBar("onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mostrarSnackBar("onDestroy")
-    }
-
     override fun onRestart() {
         super.onRestart()
-        mostrarSnackBar("onRestart")
+        mostrarSnackbar( "onRestart")
+    }
+    override fun onPause() {
+        super.onPause()
+        mostrarSnackbar("onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        mostrarSnackbar( "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarSnackbar( "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            // GUARDAR LAS VARIABLES
+            // PRIMITIVOS
+            putString("textoGuardado", textoGlobal)
+            //putInt("numeroGuardado", numero)
+        }
+        super.onSaveInstanceState(outState)
     }
 
 
 
+
+
+
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // RECUPERAR LAS VARIABLES
+        // PRIMITIVOS
+        val textoRecuperado:String? = savedInstanceState
+            .getString("textoGuardado")
+        // val textoRecuperado:Int? = savedInstanceState
+        // .getInt("numeroGuardado")
+        if(textoRecuperado!= null){
+            mostrarSnackbar(textoRecuperado)
+            textoGlobal = textoRecuperado
+        }
+    }
 }

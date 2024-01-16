7f4e7058fc78fd6be4f637ef52af42fb7ef79fa2
rafael <rafael.piedra@epn.edu.ec>
2024-01-16 15:36:02 -0500

    actualizado clase 17

--- 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/FRecyclerView.kt
+++ 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/FRecyclerView.kt
@@ -2,10 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class FRecyclerView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frecycler_view)
+        inicializarRecyclerView()
+    }
+
+    private fun inicializarRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.rv_entrenadores)
+        val adaptador = FRecyclerViewAdaptadorNombreDescripcion(
+            this,
+            BBaseDatosMemoria.arregloBEntrenador,
+            recyclerView
+        )
+        recyclerView.adapter = adaptador
+        recyclerView.itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+        recyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(this)
+        adaptador.notifyDataSetChanged()
+    }
+
+    fun aumentarTotalLikes() {
+        TODO("Not yet implemented")
     }
 }
--- 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/FRecyclerViewAdaptadorNombreDescripcion.kt
+++ 02_android/MyApplication/MyApplication/app/src/main/java/com/example/myapplication/FRecyclerViewAdaptadorNombreDescripcion.kt
@@ -1,36 +1,70 @@
 package com.example.myapplication
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.BEntrenador
+import com.example.myapplication.FRecyclerView
+import com.example.myapplication.R
 
 class FRecyclerViewAdaptadorNombreDescripcion(
     private val contexto: FRecyclerView,
     private val lista: ArrayList<BEntrenador>,
     private val recyclerView: RecyclerView
-): RecyclerView.Adapter<
+) : RecyclerView.Adapter<
         FRecyclerViewAdaptadorNombreDescripcion.MyViewHolder
-        >()  {
+        >() {
     inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(
         view
     ) {
+        val nombreTextView: TextView
+        val descripcionTextView: TextView
+        val likesTextView: TextView
+        val accionButton: Button
+        var numeroLikes = 0
+
+        init {
+            nombreTextView = view.findViewById(R.id.tv_nombre)
+            descripcionTextView = view.findViewById(R.id.tv_descripcion)
+            likesTextView = view.findViewById(R.id.tv_likes)
+            accionButton = view.findViewById(R.id.btn_dar_like)
+            accionButton.setOnClickListener { anadirLike() }
+        }
+
+        fun anadirLike() {
+            numeroLikes = numeroLikes + 1
+            likesTextView.text = numeroLikes.toString()
+            contexto.aumentarTotalLikes()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
     override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): FRecyclerViewAdaptadorNombreDescripcion.MyViewHolder {
-        TODO("Not yet implemented")
+        parent:
+        ViewGroup, viewType: Int
+    ): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.recycler_view_vista,
+            parent,
+            false
+        )
+        return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(
         holder: FRecyclerViewAdaptadorNombreDescripcion.MyViewHolder,
         position: Int
     ) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val entrenadorActual = this.lista[position]
+        holder.nombreTextView.text = entrenadorActual.nombre
+        holder.descripcionTextView.text = entrenadorActual.descripcion
+        holder.likesTextView.text = "0"
+        holder.accionButton.text = "ID:${entrenadorActual.id} Nombre: ${entrenadorActual.nombre}"
     }
 }
